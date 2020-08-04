d620ea000978476e7e87625da01d44d986ed6772
Kayros <eduardado@gmail.com>
2020-08-04 07:41:41 +0200

    plantilla para apuntes

---
+++ 00plantilla-apuntes.js
@@ -0,0 +1,13 @@
+/*  # Project Name
+    ## Lección
+    Start:  xx/xx/20xx
+    Pomodoros:  
+    Notas: 
+        - "Better Comments" ; Visual Studio Code
+        - Uso comentarios con colores */
+    
+
+//*==MAP========================================================-->
+//?==SINTAXIS===================================================-->
+//todo==Teoría==================================================-->
+//!=SNIPPETS===================================================-->
